aac0653fe86dcbf26edaed39e54e2e6f46609113
Sai Mohan <saimohan.hyle@thundersoft.com>
2022-07-06 23:07:22 +0530

    Python Programs

---
+++ functions.py
@@ -0,0 +1,28 @@
+
+
+
+
+a=20
+def test():
+    global a
+    a=a+20
+    print(a)
+    return
+test()
+
+
+
+
+def fun(name):
+    print(name + " TS")
+
+fun("India")
+fun("China")
+
+
+
+def fun(name,name1):
+    print("TS",name + name1)
+
+fun("India ","hyderabad")
+fun("China ","Beiging")
---
+++ id_fun.py
@@ -0,0 +1,21 @@
+x=20
+y=10+10
+print(x is y)
+
+#is will compare id
+
+
+x=10
+y=20
+print(x==y)
+#== will compare value
+
+
+
+print(id(x))
+print(id(y))
+
+print(id(2)-id(1))
+
+
+print(id(y)-id(x))
